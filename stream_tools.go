af8d73e90e8e0d17615e96c355e57bfb0f083370
Vasilii Pavlov <pnvasko@gmail.com>
2025-08-11 15:09:05 +0200

    add WithAllowMsgTTL two stream

--- stream_tools.go
+++ stream_tools.go
@@ -2,19 +2,21 @@ package nats_jetstream_flow
 
 import (
 	"context"
+
 	"github.com/nats-io/nats.go/jetstream"
 )
 
 func CreateOrUpdateStream(ctx context.Context, js jetstream.JetStream, config StreamConfiguration) (jetstream.Stream, error) {
 	streamConfig := jetstream.StreamConfig{
-		Name:       config.StreamName(),
-		Subjects:   config.Subjects(),
-		MaxAge:     config.CleanupTTL(),
-		Discard:    config.DiscardPolicy(),
-		Retention:  config.RetentionPolicy(),
-		Storage:    config.StreamStorageType(),
-		Replicas:   config.StreamReplicas(),
-		Duplicates: config.StreamDuplicatesWindow(),
+		Name:        config.StreamName(),
+		Subjects:    config.Subjects(),
+		MaxAge:      config.CleanupTTL(),
+		Discard:     config.DiscardPolicy(),
+		Retention:   config.RetentionPolicy(),
+		Storage:     config.StreamStorageType(),
+		Replicas:    config.StreamReplicas(),
+		Duplicates:  config.StreamDuplicatesWindow(),
+		AllowMsgTTL: config.AllowMsgTTL(),
 	}
 
 	if config.DiscardPolicy() == jetstream.DiscardNew && config.MaxMsgsPerSubject() > 0 {
